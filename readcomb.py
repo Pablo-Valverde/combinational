21f235365ac7c4a1c6a1b0afc687f782dc98b6ae
Pablo-Valverde <pablo.valver@alu.uclm.es>
2021-11-28 16:29:20 +0100

    Delete readcomb.py

--- readcomb.py
+++
@@ -1,5 +0,0 @@
-with open("E:\Programs\\test\contras.txt", "r") as file:
-        for line in file:
-                if line[:-1] == "lamadre12":
-                        print("got'em")
-                        break
