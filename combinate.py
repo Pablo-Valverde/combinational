98c7db0e7aa58d438b544ba4ae3e82f9fa5679b4
Pablo-Valverde <pablo.valver@alu.uclm.es>
2021-11-28 16:26:00 +0100

    Update combinate.py

--- combinate.py
+++ combinate.py
@@ -12,18 +12,56 @@ def calc_iterations(characters, depth):
 
 def combinate(characters, depth = 5, prefix = "", func = print):
         total_iterations = calc_iterations(characters, depth)
-        return __combinate(characters, depth, prefix, func)
+        __combinate(characters, depth, prefix, func)
+        return total_iterations
 
-def __combinate(characters, depth = 5, prefix = "", func = print, iterations = 0):
+def __combinate(characters, depth = 5, prefix = "", func = print):
         for c in characters:
                 prefix += c
                 func(prefix)
                 if depth > 1:
-                        iterations = __combinate(characters, depth - 1 , prefix, func, iterations)
-                prefix = prefix[:-1]
-        return iterations
+                        __combinate(characters, depth - 1 , prefix, func)
+                prefix = prefix[:-c.__len__()]
 
 #----Auxiliar -> to another proyect----#
+
+#----Dict combinate----#
+
+import _thread
+from os import close
+
+__keywords = set()
+__buffer = ""
+file = open("E:\Programs\\test\contras.txt", "w")
+
+def addword(word):
+        split = word
+        ind = 0
+        offset = split.__len__()-1
+        while split.__len__() != 0:
+                __keywords.add(split)
+                if (ind + offset) > word.__len__():
+                        ind = 0
+                        offset -= 1
+                split = word[ind:ind + offset]
+                ind += 1
+
+def addwords(words):
+        for word in words:
+                addword(word)
+
+def func(comb):
+        global __buffer
+        __buffer += comb + "\n"
+        if __buffer.split("\n").__len__() > 10000:
+                _thread.start_new_thread(writeb, ())
+
+def writeb():
+        global __buffer
+        file.write(__buffer)
+        __buffer = ""
+
+#----Hash paswords----#
 
 import hashlib
 
@@ -61,8 +99,8 @@ def sha256(s):
     dig_hash = und_hash.digest().decode("Latin1")
     return dig_hash
 
-with open("D:\Programs\hash\hashes.txt", "w") as file:
-        pass
-
-print(combinate(generate_comb(' ', '~'), 5, func=hash))
-write()
+phrase = "sacarmela for the win madre 1 2 3 4 5 6 7 8 9 0"
+addwords(phrase.split(" "))
+combinate(__keywords, 4, func=func)
+writeb()
+file.close()
